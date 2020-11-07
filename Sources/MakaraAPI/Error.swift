c3fb0992e009c63b48f955798e245544e1b241d7
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-11-07 16:54:06 +1100

    add tech message

--- Sources/MakaraAPI/Error.swift
+++ Sources/MakaraAPI/Error.swift
@@ -11,11 +11,13 @@ import Foundation
 public protocol MakaraError: Identifiable, Error, CustomStringConvertible {
     
     var message: String { get }
+    var technicalMessage: String? { get }
     
 }
 
 extension MakaraError {
     
-    public var description: String { get { return self.message }}
+    public var description: String { get { return self.message } }
+    public var technicalMessage: String? { get { return nil } }
     
 }
