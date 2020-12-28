09f35c268f876797a35842858ffb62c8af9525b1
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-12-29 09:12:54 +1100

    add `Activity.name`

--- Sources/MakaraAPI/Expedition/Activity.swift
+++ Sources/MakaraAPI/Expedition/Activity.swift
@@ -13,5 +13,16 @@ public enum Activity: Int, Codable {
     case dive = 1
     case snorkel = 2
     case swim = 3
+    
+    public var name: String { get {
+        switch self {
+        case .dive:
+            return "dive"
+        case .snorkel:
+            return "snorkel"
+        case .swim:
+            return "swim"
+        }
+    } }
 
 }
