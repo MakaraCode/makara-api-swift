d9f2665db3c445ead49462c50117122247c23abc
Hugh Jeremy <hugh.jeremy@gmail.com>
2021-01-23 15:58:13 +1100

    add DiveSiteProfile demo extension

---
+++ Sources/MakaraAPI/DemoData/SiteProfile_Demo.swift
@@ -0,0 +1,22 @@
+//
+//  SiteProfile_Demo.swift
+//  
+//
+//  Created by Hugh Jeremy on 23/1/21.
+//
+
+import Foundation
+
+
+extension DiveSiteProfile {
+    
+    public static let demoSite1 = DiveSiteProfile(
+        site: DiveSite.demoSite1,
+        shopId: Shop.demoShop.publicId,
+        name: "Comet's Hole",
+        description: nil,
+        offered: true,
+        disposition: nil
+    )
+
+}
